9275a14306ee6a7dd050d7278df36469ce08158b
Sasha <example@gmail.com>
2021-06-15 14:21:26 +0300

    interaction

---
+++ UserInteraction/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert('Hello');
+
+// let result = confirm("Yes Or No");
+
+
+// const answ = prompt("Are you an adult?", "Hm?"); // Always string
+
+const answers = [];
+
+answers[0] = prompt("", '');
+
+document.write(answers);
+
+// null --> object (mistake)
