b9d0ce4c46a89b88295f3e01df1e614091da0a0f
Sasha <example@gmail.com>
2021-06-15 14:50:18 +0300

    code

---
+++ Interpolation_operators/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Interpolation
+
+const ctg = 'toys';
+
+console.log(`https://url.com/${ctg}`);
+
+// Operators 
+
+console.log(4 + +"6");
+
+let incr = 2, 
+    decr = 4;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(7 % 3);
+
+const a = true, b = false;
+
+console.log(a && b);
+
+
+console.log( 3 || 2 && 5); // 3
+
+
