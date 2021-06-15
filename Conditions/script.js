5c6f8b7530876fb8ca29c0249111aa4d9ce896f7
Sasha <example@gmail.com>
2021-06-15 17:32:06 +0300

    Conditions

---
+++ Conditions/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+if (4 == 5) {
+    console.log('true');
+}
+else {
+    console.log('false');
+}
+
+const number = 100;
+
+if (number < 99) {
+    console.log('Less than 100')
+} else {
+    console.log('Bigger than 100 or equal');
+}
+
+let num = 50;
+(num == 50) ? console.log('cool') : console.log('bad');
+
+switch (num) {
+    case 50: console.log('Woo');
+    break;
+}
