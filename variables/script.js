750fa5474853be6226340802e11ddbe94b9e95aa
Sasha <example@gmail.com>
2021-06-15 12:56:20 +0300

    use strict

--- variables/script.js
+++ variables/script.js
@@ -1,5 +1,7 @@
+"use strict"; // Useful thing
+
 let number = 5;
-const leftBorder = 14; //CamelCase
+const leftBorder = 14; // CamelCase
 
 number = 10;
 console.log(number);
@@ -12,3 +14,9 @@ obj.a = 3;
 console.log(obj);
 
 
+{
+    let result = 50;
+}
+// var result -> no Error
+
+console.log(result); // Is not defined
