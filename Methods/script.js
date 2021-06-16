5690b00016452797c132a3894cbdc2d2e9513898
QWAR-svg <zaz765831@gmail.com>
2021-06-16 10:27:46 +0300

    methods

---
+++ Methods/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const str = "word"; 
+const array = [1, 4 , 7];
+
+// console.log(str[3] = 'q'); // Will not change
+console.log(str.replace("o", "b"));
+
+const apple = "apple";
+
+const word = "Hi world";
+
+console.log(word.slice(4, 9));
+
+console.log(apple.indexOf("pl")); // 2
+
+console.log(word.substring(4, 9));
+
+// substring arguments > 0
+// substr - second argument shows length of the word
+
+const num = 1543.2;
+
+console.log(Math.round(num));
+
+const px = "13.4px";
+
+console.log(parseInt(px)); // 13
+
+
+
