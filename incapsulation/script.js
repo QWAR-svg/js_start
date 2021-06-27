43c10d52506aa0d7a80d334f156a76191105e53b
QWAR-svg <zaz765831@gmail.com>
2021-06-27 14:40:00 +0300

    Up_again

---
+++ incapsulation/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+    
+    #surname = "Bob";
+
+    say() {
+        console.log(`Name user: ${this.name}, age: ${this._age}`);
+    }
+
+    get name() {
+        return this.#surname;
+    }
+
+    set name(age) {
+        if (typeof age  === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log("Bad");
+        }
+    }
+}
+
+const alex = new User('Alex', 15);
+
+
+// console.log(alex.getAge());
+// alex.setAge(20);
+// console.log(alex.getAge());
+console.log(alex.name);
+
+
+
